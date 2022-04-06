8f26e56e2e21666e765aa3f4daf401fe4945de2e
PrINcE <dhruvkotwani712@gmail.com>
2022-04-06 05:43:26 +0530

    done

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -87,11 +87,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -128,8 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATICFILES_DIRS=[
 STATIC_DIR,
  ]
